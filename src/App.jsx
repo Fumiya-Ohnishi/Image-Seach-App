cb4bf7ea17af2d173c178d7bc5855619c2c5ffc7
fumiya.oonishi <fumiya.oonishi@pricer.co.jp>
2022-09-13 16:06:28 +0900

    Rename files from CSS to SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import axios from "axios"
 import Title from "./components/Title"
 import Form from "./components/Form"
-import "./App.css"
+import "./App.scss"
 import Results from "./components/Results"
 import("https://fonts.googleapis.com/css2?family=Caveat&display=swap")
 
